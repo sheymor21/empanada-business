567fe6da9e1bc8dd18d5a3769caae8ba570ae954
Sheymor <joseacvz81@gmail.com>
2024-10-07 14:16:05 -0400

    Remove DatabaseProvider and inline DB config
    Refactored database configuration by deleting `DatabaseProvider` and moving its logic directly into `app.module.ts`. This simplifies the setup by keeping the configuration in one place and makes environment variable management more straightforward.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
 import {Module} from '@nestjs/common';
 import {EmpanadasModule} from './empanadas/empanadas.module';
-import {DatabaseProvider} from "./database/database.config";
 import {OrderModule} from './order/order.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import * as process from "node:process";
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+export type DbConfig = {
+    username: string;
+    password: string;
+    databaseName: string;
+    port: number;
+}
+
+const config: DbConfig = {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    databaseName: process.env.DB_NAME || "test",
+    port: Number(process.env.DB_PORT) || 3306
+}
 
 @Module({
-    imports: [EmpanadasModule, OrderModule, DatabaseProvider.get()],
+    imports: [EmpanadasModule, OrderModule, TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'localhost',
+        port: config.port,
+        username: config.username,
+        password: config.password,
+        database: config.databaseName,
+        synchronize: true,
+        autoLoadEntities: true
+    })],
     controllers: [],
     providers: [],
 })
